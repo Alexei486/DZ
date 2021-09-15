e39ed17e4628ab03feabfe2ec61c10e7860450e8
Alexei486 <81119761+Alexei486@users.noreply.github.com>
2021-09-15 16:10:25 +0300

    nomer_dwa

---
+++ dz/nomer_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+int main() {
+	std::vector<int> vec = { 7, -42, 5, 0, 16, -8 };
+	for (int j = 1; j < vec.size(); j++) {
+		int q = vec[j];
+		int p = j;
+		while (p > 0 && vec[p - 1] > vec[p]) {
+			std::cout << p << ' ';
+			std::swap(vec[p - 1], vec[p]);
+			p -= 1;
+		}
+	}
+	for (int i = 0; i < vec.size(); i++) std::cout << "\n" << vec[i] << "\n";
+	return 0;
+}
