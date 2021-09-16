5c04060b649f545a3788676ddf996668ba8fbc0f
Alexei486 <81119761+Alexei486@users.noreply.github.com>
2021-09-16 16:16:44 +0300

    Update nomer_2.cpp
    dfewfe

--- dz/nomer_2.cpp
+++ dz/nomer_2.cpp
@@ -2,13 +2,11 @@
 #include <vector>
 int main() {
 	std::vector<int> vec = { 7, -42, 5, 0, 16, -8 };
-	for (int j = 1; j < vec.size(); j++) {
-		int q = vec[j];
+	for (int j = 1; j < std::size(vec); j++) {
 		int p = j;
-		while (p > 0 && vec[p - 1] > vec[p]) {
-			std::cout << p << ' ';
+		for (p; p>0 && vec[p - 1] > vec[p];--p)
+		{
 			std::swap(vec[p - 1], vec[p]);
-			p -= 1;
 		}
 	}
 	for (int i = 0; i < vec.size(); i++) std::cout << "\n" << vec[i] << "\n";
