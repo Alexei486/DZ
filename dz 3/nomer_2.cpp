5b7304435b0a5c8e577e8e0d4570a8d343596670
Alexei486 <81119761+Alexei486@users.noreply.github.com>
2021-09-23 23:35:26 +0300

    Update nomer_2.cpp
    уа

--- dz 3/nomer_2.cpp
+++ dz 3/nomer_2.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
 #include <functional>
 #include <vector>
-double calculate(double x1, double x2, std::function < double(double, double)> A)
+double calculate(double x1, double x2, std::function < double(double, double)>a)
 {
-	return A(x1, x2);
+	return a(x1, x2);
 }
 
 int main()
 {
-	auto mem = [](double x1, double x2) {return x1 + x2; };
-	auto mem1 = [](double x1, double x2) {return x1 * x2; };
-	auto mem2 = [](double x1, double x2) {return x1 - x2; };
-	auto mem3 = [](double x1, double x2) {return x1 / x2; };
 	double x1 = 1.23;
 	double x2 = 4.56;
-	std::vector <std::function < double(double, double) >> function { mem,mem1,mem2,mem3 };
-	for (auto &dd:function)
+	std::vector <std::function < double(double, double) >> function { [](double x1, double x2) {return x1 + x2; } ,[](double x1, double x2) {return x1 * x2; },[](double x1, double x2) {return x1 - x2; }, [](double x1, double x2) {return x1 / x2; } };
+	for (auto dd:function)
 	{
 		std::cout << calculate(x1,x2,dd)<< std::endl;
 	}
