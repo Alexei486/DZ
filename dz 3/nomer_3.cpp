a52f33afcbf087990aa609771b085c47d214fea2
Alexei486 <81119761+Alexei486@users.noreply.github.com>
2021-09-24 01:12:34 +0300

    Add files via upload
    wefw

--- dz 3/nomer_3.cpp
+++ dz 3/nomer_3.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+std::vector <int> ascending(std::vector <int> vec)
+{
+    [vec]()
+    {
+        std::sort(std::begin(vec), std::end(vec), [](int a, int b) {return a > b; });
+    };
+    for (auto i : vec) {std::cout << i << " ";}
+    return (vec);
+}
+std::vector <int>  descending(std::vector <int> vec)
+{
+    [vec]() {std::sort(std::begin(vec),std::end(vec), [](int a, int b) {return a < b;});
+    };
+    for (auto i : vec) { std::cout << i << " "; };
+    return (vec);
+}
 int main()
 {
-    std::vector<int> vec = { 7, -42, 5, 0, 16, -8 };
-    std::sort(std::begin(vec), std::end(vec), [](auto a, auto b) {return a > b; });
-    for (auto i : vec) { std::cout << i << " "; }
-    std::sort(std::begin(vec), std::end(vec), [](auto a,auto b) {return a < b; });
-    for (auto i : vec) { std::cout << i << " "; };
-
-    return 0;
+	std::vector<int> vec = { 7, -42, 5, 0, 16, -8 };
+    ascending(vec);
+    descending(vec);
+    cout << "llol";
+	return 0;
 }
