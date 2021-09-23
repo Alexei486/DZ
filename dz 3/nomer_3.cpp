76b84b22e96462b88e2038cf7df0731779fc1ebb
Alexei486 <81119761+Alexei486@users.noreply.github.com>
2021-09-24 00:28:04 +0300

    Update nomer_3.cpp
    wefew

--- dz 3/nomer_3.cpp
+++ dz 3/nomer_3.cpp
@@ -5,9 +5,10 @@
 int main()
 {
     std::vector<int> vec = { 7, -42, 5, 0, 16, -8 };
-    [&vec](){std::sort(std::begin(vec), std::end(vec), [](auto a, auto b) {return a > b; });}();
+    std::sort(std::begin(vec), std::end(vec), [](auto a, auto b) {return a > b; });
     for (auto i : vec) { std::cout << i << " "; }
-    [&vec]() {std::sort(std::begin(vec), std::end(vec), [](auto a,auto b) {return a < b; });}();
+    std::sort(std::begin(vec), std::end(vec), [](auto a,auto b) {return a < b; });
     for (auto i : vec) { std::cout << i << " "; };
+
     return 0;
 }
