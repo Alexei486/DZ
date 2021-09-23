f567e12dcf6ea52f1f50388a7000c4f8d818c11e
Alexei486 <81119761+Alexei486@users.noreply.github.com>
2021-09-23 22:47:42 +0300

    Update nomer_2.cpp
    wd

--- dz 3/nomer_2.cpp
+++ dz 3/nomer_2.cpp
@@ -1,30 +1,19 @@
 #include <iostream>
 #include <functional>
 #include <vector>
-std::function < double(double, double) > mem = [](double x1, double x2)
-{
-	return x1 + x2;
-};
-std::function < double(double, double) > mem1 = [](double x1, double x2)
-{
-	return x1 * x2;
-};
-std::function < double(double, double) > mem2 = [](double x1, double x2)
-{
-	return x1 - x2;
-};
-std::function < double(double, double) > mem3 = [](double x1, double x2)
-{
-	return x1 / x2;
-};
+
 int main()
 {
+	std::function < double(double, double) > mem = [](double x1, double x2) {return x1 + x2; };
+	std::function < double(double, double) > mem1 = [](double x1, double x2) {return x1 * x2; };
+	std::function < double(double, double) > mem2 = [](double x1, double x2) {return x1 - x2; };
+	std::function < double(double, double) > mem3 = [](double x1, double x2) {return x1 / x2; };
 	double x1 = 1.23;
 	double x2 = 4.56;
 	std::vector <std::function < double(double, double) >> functions{ mem,mem1,mem2,mem3 };
-	std::cout << (mem)(x1, x2) << std::endl;
-	std::cout << (mem1)(x1, x2) << std::endl;
-	std::cout << (mem2)(x1, x2) << std::endl;
-	std::cout << (mem3)(x1, x2) << std::endl;
+	for (int i = 0;i<4; i++)
+	{
+		std::cout << functions[i](x1,x2)<<std::endl;
+	}
 	return 0;
 }
