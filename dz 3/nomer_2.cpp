32600ba1c04ae992de5b08054ddb1d310893eb80
Alexei486 <81119761+Alexei486@users.noreply.github.com>
2021-09-23 23:08:24 +0300

    Update nomer_2.cpp
    фыыф

--- dz 3/nomer_2.cpp
+++ dz 3/nomer_2.cpp
@@ -4,16 +4,16 @@
 
 int main()
 {
-	std::function < double(double, double) > mem = [](double x1, double x2) {return x1 + x2; };
-	std::function < double(double, double) > mem1 = [](double x1, double x2) {return x1 * x2; };
-	std::function < double(double, double) > mem2 = [](double x1, double x2) {return x1 - x2; };
-	std::function < double(double, double) > mem3 = [](double x1, double x2) {return x1 / x2; };
+	auto mem = [](double x1, double x2) {return x1 + x2; };
+	auto mem1 = [](double x1, double x2) {return x1 * x2; };
+	auto mem2 = [](double x1, double x2) {return x1 - x2; };
+	auto mem3 = [](double x1, double x2) {return x1 / x2; };
 	double x1 = 1.23;
 	double x2 = 4.56;
-	std::vector <std::function < double(double, double) >> functions{ mem,mem1,mem2,mem3 };
-	for (int i = 0;i<4; i++)
+	std::vector <std::function < double(double, double) >> calculate { mem,mem1,mem2,mem3 };
+	for (int i = 0; i < 4; i++)
 	{
-		std::cout << functions[i](x1,x2)<<std::endl;
+		std::cout << calculate[i](x1, x2) << std::endl;
 	}
 	return 0;
 }
