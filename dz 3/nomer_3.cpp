baaa1cfb09dc519452d944505f2c2495453b51a8
Alexei486 <81119761+Alexei486@users.noreply.github.com>
2021-09-24 00:22:08 +0300

    Update nomer_3.cpp
    weed

--- dz 3/nomer_3.cpp
+++ dz 3/nomer_3.cpp
@@ -1,26 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-std::vector <int> ascending(std::vector <int> vec)
-{
-    [&vec]()
-    {
-        std::sort(std::begin(vec), std::end(vec), [](int a, int b) {return a > b; });
-    }();
-    for (auto i : vec) {std::cout << i << " ";}
-    return (vec);
-}
-std::vector <int>  descending(std::vector <int> vec)
-{
-    [&vec]() {std::sort(std::begin(vec),std::end(vec), [](int a, int b) {return a < b;});
-    }();
-    for (auto i : vec) { std::cout << i << " "; };
-    return (vec);
-}
+
 int main()
 {
-	std::vector<int> vec = { 7, -42, 5, 0, 16, -8 };
-    ascending(vec);
-    descending(vec);
-	return 0;
+    std::vector<int> vec = { 7, -42, 5, 0, 16, -8 };
+    [&vec](){std::sort(std::begin(vec), std::end(vec), [](auto a, auto b) {return a > b; });}();
+    for (auto i : vec) { std::cout << i << " "; }
+    [&vec]() {std::sort(std::begin(vec), std::end(vec), [](auto a,auto b) {return a < b; });}();
+    for (auto i : vec) { std::cout << i << " "; };
+    return 0;
 }
