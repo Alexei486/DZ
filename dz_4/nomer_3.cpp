e17a27b0cb97b77cf4173b9aa418cb0e27eb754d
Alexei486 <81119761+Alexei486@users.noreply.github.com>
2021-10-08 02:27:29 +0300

    Update nomer_3.cpp
    backks

--- dz_4/nomer_3.cpp
+++ dz_4/nomer_3.cpp
@@ -2,21 +2,20 @@
 
 struct times
 {
-	int year : 12;
-	int month : 5;
-	int day : 6;
-	int H : 7;
-	int min : 7;
-	int sec : 7;
-	int msec : 11;
+	unsigned int year : 11;
+	unsigned int month : 4;
+	unsigned int day :5;
+	unsigned int H : 6;
+	unsigned int min : 6;
+	unsigned int sec : 6;
+	unsigned int msec : 10;
 };
 void printInformation(times good)
 {
-	std::cout << "Data1: "<<good.year<<":" << good.month << ":" << good.day << ":" << good.H << ":" << good.min<< ":" << good.sec << ":" << good.msec << ":";
+	std::cout << "Data1: " << good.year << ":" << good.month << ":" << good.day << ":" << good.H << ":" << good.min << ":" << good.sec << ":" << good.msec << std::endl;
 }
 int main()
 {
-	times day1{  2021,12,31,57,59,2,134 };
+	times day1{ 2020,11,30,58,58,58,998 };
 	printInformation(day1);
-
 }
