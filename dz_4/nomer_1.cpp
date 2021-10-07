24927ac9fb961653f586b20934eb7f405fc3cec7
Alexei486 <81119761+Alexei486@users.noreply.github.com>
2021-10-08 02:24:20 +0300

    Update nomer_1.cpp
    errors

--- dz_4/nomer_1.cpp
+++ dz_4/nomer_1.cpp
@@ -10,11 +10,24 @@ struct boss
 	std::string name;
 	int health;
 	int drop;
+	void text()
+	{
+		std::cout << "BOSS SUMMONED" <<std::endl;
+	}
 };
 
+struct pet2
+{
 
+	std::string name;
+	void pikachu(int& weapon)
+	{
+		weapon++;
+		system("cls");
+	}
+};
 
-void fight(int weapon, int pet, int money)
+void fight(int& weapon, int& pet, double & money)
 {
 	std::mt19937 gen;
 	gen.seed(time(0));
@@ -31,8 +44,8 @@ void fight(int weapon, int pet, int money)
 		system("cls");
 		if (m == "Cheat")
 		{
-		money = 0;
-		std::cout << "Go away, Hacker!" << "\n";
+			money = 0;
+			std::cout << "Go away, Hacker!" << "\n";
 		}
 		else
 		{
@@ -41,9 +54,9 @@ void fight(int weapon, int pet, int money)
 				debuff += gen() % 2;
 				std::cout << "Mob gave you debuff: " << debuff << "\n";
 				std::cout << "Mob Health: " << health << "\n";
-				std::cout << "summary attack: " << weapon + pet - debuff<< "\n";
+				std::cout << "summary attack: " << weapon + pet - debuff << "\n";
 				std::cout << "you have " + std::to_string(move) + " moves left" << "\n";
-				health = health - weapon - pet;
+				health -= (health + weapon+ pet);
 				--move;
 			}
 			else
@@ -52,9 +65,7 @@ void fight(int weapon, int pet, int money)
 				std::cout << "You won! ";
 				break;
 			}
-
 		}
-
 	}
 	if (health > 0)
 	{
@@ -64,7 +75,7 @@ void fight(int weapon, int pet, int money)
 	}
 	else
 	{
-		std::cout << "You got: " << "\n" <<drop << "Money" << "\n";
+		std::cout << "You got: " << "\n" << drop << "Money" << "\n";
 		money += drop;
 		std::this_thread::sleep_for(std::chrono::seconds(3));
 		system("cls");
@@ -72,7 +83,7 @@ void fight(int weapon, int pet, int money)
 }
 
 
-void to_spam(double money, int weapon, int pet)
+void to_spam(double& money, int& weapon, int& pet)
 {
 	std::cout << "Money: " << money << "\n";
 	std::cout << "Weapon attack: " << weapon << "\n";
@@ -83,39 +94,38 @@ void to_spam(double money, int weapon, int pet)
 	std::cout << "to enter the boss fight write: Boss " << "\n";
 	std::cout << "to enter the boss fight write: Fight " << "\n";
 	std::cout << "________________________________________" << "\n";
-
-
-
-
-
 	return;
 }
-
-void Shop(double* money, int* weapon, int* upgrade1, int* upgrade2, double* cost, int* pet)
+void Shop(double &money, int &weapon, int &upgrade1, int &upgrade2, double &cost, int &pet , struct pet2 PIKACHU)
 {
 	std::string Text_Shop;
-	std::cout << "Money: " << *money << "\n";
-	std::cout << "Weapon attack: " << *weapon << "\n";
-	std::cout << "Pet attack: " << *pet << "\n";
-	std::cout << "improve weapon(+1 attack) cost: " << pow(1.5, *upgrade1) * *cost << "\n";
-	std::cout << "improve pet(+1 attack) cost: " << pow(2, *upgrade2) * *cost << "\n";
+	std::cout << "Money: " << money << "\n";
+	std::cout << "Weapon attack: " << weapon << "\n";
+	std::cout << "Pet attack: " << pet << "\n";
+	std::cout << "improve weapon(+1 attack) cost: " << pow(1.5, upgrade1) * cost << "\n";
+	std::cout << "improve pet(+1 attack) cost: " << pow(2, upgrade2) * cost << "\n";
 	std::cout << "To purchase enter: Buy weapon " "\n";
 	std::cout << "To purchase enter: Buy pet " "\n";
 	std::cout << "To exit the store, enter: Back " "\n";
 	std::cout << "________________________________________" << "\n";
 	std::getline(std::cin, Text_Shop);
+	if (Text_Shop == "PIKA")
+	{
+		PIKACHU.pikachu(weapon);
+		Shop(money, weapon, upgrade1, upgrade2, cost, pet, (pet2(PIKACHU)));
+	}
 	if (Text_Shop == "Buy pet")
 	{
-		if (*money > pow(1.5, *upgrade2) * *cost)
+		if (money > pow(1.5, upgrade2) * cost)
 		{
-			*money -= pow(1.5, *upgrade2) * *cost;
-			*pet += 1;
-			*upgrade2 += 1;
+			money -= pow(1.5, upgrade2) * cost;
+			pet ++;
+			upgrade2++;
 			system("cls");
 			std::cout << "\n" << "\n" << "Thank you for your purchase";
 			std::this_thread::sleep_for(std::chrono::seconds(1));
 			system("cls");
-			Shop(money, weapon, upgrade1, upgrade2, cost, pet);
+			Shop(money, weapon, upgrade1, upgrade2, cost, pet, (pet2(PIKACHU)));
 		}
 		else
 		{
@@ -123,21 +133,21 @@ void Shop(double* money, int* weapon, int* upgrade1, int* upgrade2, double* cost
 			std::cout << "\n" << "\n" << "Not enough money";
 			std::this_thread::sleep_for(std::chrono::seconds(1));
 			system("cls");
-			Shop(money, weapon, upgrade1, upgrade2, cost, pet);
+			Shop(money, weapon, upgrade1, upgrade2, cost, pet, (pet2(PIKACHU)));
 		}
 	}
 	if (Text_Shop == "Buy weapon")
 	{
-		if (*money > pow(1.5, *upgrade1) * *cost)
+		if (money > pow(1.5, upgrade1) * cost)
 		{
-			*money -= pow(1.5, *upgrade1) * *cost;
-			*weapon += 1;
-			*upgrade1 += 1;
+			money -= pow(1.5, upgrade1) * cost;
+			weapon ++;
+			upgrade1 ++;
 			system("cls");
 			std::cout << "\n" << "\n" << "Thank you for your purchase";
 			std::this_thread::sleep_for(std::chrono::seconds(1));
 			system("cls");
-			Shop(money, weapon, upgrade1, upgrade2, cost, pet);
+			Shop(money, weapon, upgrade1, upgrade2, cost, pet, (pet2(PIKACHU)));
 		}
 		else
 		{
@@ -145,7 +155,7 @@ void Shop(double* money, int* weapon, int* upgrade1, int* upgrade2, double* cost
 			std::cout << "\n" << "\n" << "Not enough money";
 			std::this_thread::sleep_for(std::chrono::seconds(1));
 			system("cls");
-			Shop(money, weapon, upgrade1, upgrade2, cost, pet);
+			Shop(money, weapon, upgrade1, upgrade2, cost, pet, (pet2(PIKACHU)));
 		}
 	}
 	if (Text_Shop == "Back")
@@ -155,32 +165,33 @@ void Shop(double* money, int* weapon, int* upgrade1, int* upgrade2, double* cost
 	}
 	return;
 }
-void start_game(double* money, int* weapon, int* pet, double* cost, int* upgrade1, int* upgrade2, struct boss tom, struct  boss ivan)
+void start_game(double& money, int& weapon, int& pet, double& cost, int& upgrade1, int& upgrade2, struct boss tom, struct  boss ivan,struct pet2 PIKACHU)
 {
 	const auto n = 1;
 	std::string m;
 	while (n > -10)
 	{
-		to_spam(*money, *weapon, *pet);
+		to_spam(money, weapon, pet);
 		std::getline(std::cin, m);
 		system("cls");
 		if (m == "Shop")
 		{
-			Shop(money, weapon, upgrade1, upgrade2, cost, pet);
+			Shop(money, weapon, upgrade1, upgrade2, cost, pet, (pet2(PIKACHU)));
 		}
 		if (m == "Fight")
 		{
-			fight(*weapon, *pet, *money);
+			fight(weapon, pet, money);
 		}
 		if (m == "Boss")
 		{
-			std::cout << "You have exactly one attempt to pass the test questions " <<"\n";
+			tom.text();
+			std::cout << "You have exactly one attempt to pass the test questions " << "\n";
 			std::this_thread::sleep_for(std::chrono::seconds(3));
-			std::cout << "your attack in the boss fight(10*Weapon): " << 10 * *weapon << "\n";
+			std::cout << "your attack in the boss fight(10*Weapon): " << 10 * weapon << "\n";
 			std::this_thread::sleep_for(std::chrono::seconds(3));
-			std::cout << "Boss name : " << tom.name << "\n" << "Boss health: " << tom.health << "\n" << "Drop from the boss: " <<tom.drop << "\n";
+			std::cout << "Boss name : " << tom.name << "\n" << "Boss health: " << tom.health << "\n" << "Drop from the boss: " << tom.drop << "\n";
 			std::this_thread::sleep_for(std::chrono::seconds(3));
-			if (tom.health < 10 * *weapon)
+			if (tom.health < 10 * weapon)
 			{
 				system("cls");
 				std::cout << "________________________________________" << "\n";
@@ -192,12 +203,12 @@ void start_game(double* money, int* weapon, int* pet, double* cost, int* upgrade
 				std::cout << "Fine. But that is not all";
 				std::this_thread::sleep_for(std::chrono::seconds(3));
 				system("cls");
-				std::cout << "Boss name : " <<ivan.name << "\n" << "Boss health: " << ivan.health << "\n" << "Drop from the boss: " << ivan.drop << "\n";
+				std::cout << "Boss name : " << ivan.name << "\n" << "Boss health: " << ivan.health << "\n" << "Drop from the boss: " << ivan.drop << "\n";
 				std::this_thread::sleep_for(std::chrono::seconds(3));
-				if (ivan.health < 10 * *weapon)
+				if (ivan.health < 10 * weapon)
 				{
-					*money += ivan.drop;
-					*money += tom.drop;
+					money += ivan.drop;
+					money += tom.drop;
 					std::cout << "________________________________________" << "\n";
 					std::this_thread::sleep_for(std::chrono::seconds(3));
 					std::cout << "There is an attack..." << "\n";
@@ -219,8 +230,8 @@ void start_game(double* money, int* weapon, int* pet, double* cost, int* upgrade
 					std::cout << "________________________________________" << "\n";
 					std::this_thread::sleep_for(std::chrono::seconds(3));
 					system("cls");
-					std::cout << "YOU LOSE!"<<"\n";
-					*money += 15;
+					std::cout << "YOU LOSE!" << "\n";
+					money += 15;
 					std::cout << "YOU GOT: 15 POINTS FOR KW" << "\n";
 					std::this_thread::sleep_for(std::chrono::seconds(5));
 
@@ -237,24 +248,25 @@ void start_game(double* money, int* weapon, int* pet, double* cost, int* upgrade
 				std::this_thread::sleep_for(std::chrono::seconds(3));
 				system("cls");
 				std::cout << "YOU LOSE!";
-				*money += 24;
+				money += 24;
 				std::cout << "YOU GOT: 24 POINTS FOR KW" << "\n";
 				std::this_thread::sleep_for(std::chrono::seconds(5));
 				system("cls");
 			}
-
 		}
 		else
 		{
-			*money += *weapon;
+			money += weapon;
 		}
 	}
-	start_game(money, weapon, pet, cost,upgrade1, upgrade2, (boss(tom)), (boss(ivan)));
+	start_game(money, weapon, pet, cost, upgrade1, upgrade2, (boss(tom)), (boss(ivan)),pet2(PIKACHU));
 	return;
 }
 
+
 int main()
 {
+	pet2 PIKACHU{"PIKACHU"};
 	boss tom{ "Timur_squire_gungsta",100,1000 };
 	boss ivan{ "Ivan_SENSEI_GUNGSTA", 125,2000 };
 	auto pet = 0;
@@ -263,6 +275,6 @@ int main()
 	auto upgrade2 = 1;
 	auto weapon = 15;
 	auto money = 0.0;
-	start_game(&money, &weapon, &pet, &cost, &upgrade1, &upgrade2, (boss(tom)), (boss(ivan)));
+	start_game(money, weapon, pet, cost, upgrade1, upgrade2, (boss(tom)), (boss(ivan)),(pet2(PIKACHU)));
 	return 0;
 }
