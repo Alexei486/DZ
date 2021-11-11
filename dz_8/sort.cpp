66d2dac669716853d712a1ee8f754a7ffb5f459b
Alexei486 <81119761+Alexei486@users.noreply.github.com>
2021-11-12 00:07:50 +0300

    qwd

--- dz_8/sort.cpp
+++ dz_8/sort.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <vector>
-template<typename T, size_t N>
-void TopDownMerge(T(&array)[N] ,int iBegin, int iMiddle, int iEnd)
+template<typename T, size_t N,typename F>
+void TopDownMerge(T(&array)[N], int iBegin, int iMiddle, int iEnd, F func)
 {
 	int i = iBegin, j = iMiddle;
 	int B_COPY = iEnd - iBegin;
 	std::vector <int> B(B_COPY);
 	for (auto k = 0; k < B_COPY; ++k)
 	{
-		if (i < iMiddle && (j >= iEnd || array[i] <= array[j]))
+		if (i < iMiddle && (j >= iEnd || func(array[i], array[j])))
 		{
 			B[k] = array[i];
 			++i;
@@ -24,25 +24,33 @@ void TopDownMerge(T(&array)[N] ,int iBegin, int iMiddle, int iEnd)
 		array[iBegin + j] = B[j];
 	}
 }
-template<typename T,size_t N>
-void TopDownSplitMerge(T(&array)[N],int left, int right)
+template<typename T, size_t N, typename F>
+void TopDownSplitMerge(T(&array)[], int left, int right, F func)
 {
 	if (right - left <= 1)
 	{
 		return;
 	}
 	int middle = (left + right) / 2;
-	TopDownSplitMerge(array,left, middle );
-	TopDownSplitMerge(array,middle, right );
-	TopDownMerge(array, left, middle, right);
+	TopDownSplitMerge(array, left, middle, func);
+	TopDownSplitMerge(array, middle, right, func);
+	TopDownMerge(array, left, middle, right,func);
 }
 int main()
 {
 	int a[6] = { 7, -42, 5, 0, 16, -8 };
-	TopDownSplitMerge(a, 0, 6);
+	TopDownSplitMerge(a, 0, 6, [](int a, int b) {return a <= b; });
 	for (int i = 0; i < 6; ++i)
 	{
-		std::cout << a[i]<<"\n";
+		std::cout << a[i] << "\n";
 	}
+
+	int* b = new int[7]{ 7, -42, 5, 0, 16, -8 };
+	TopDownSplitMerge(b, 0, 4, [](int a, int b) {return a <= b; });
+	for (auto i = 0; i <6; i++)
+	{
+		std::cout << b[i] << std::endl;
+	}
+		
 	return 0;
 }
